1e29d2ad6cfacc071d8e93b1cf7757863f387cd6
Bruno Parpagnoli <brnjparpagnoli@gmail.com>
2018-03-20 20:01:41 +1100

    Login and Logout methods/routes created

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,6 +10,8 @@ class Page
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -29,7 +31,10 @@ class Page
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"] === true) 
+		{
+			$this->tpl->draw("header");
+		}
 
 	}
 
@@ -57,7 +62,10 @@ class Page
 	public function __destruct()
 	{
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"] === true) 
+		{
+			$this->tpl->draw("footer");
+		}
 
 	}
 
